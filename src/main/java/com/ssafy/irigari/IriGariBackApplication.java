bd315d203611b778a7810243c658598efd481064
stack0801 <stack0801@gmail.com>
2023-11-13 15:06:08 +0900

    first commit

---
+++ src/main/java/com/ssafy/irigari/IriGariBackApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.irigari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IriGariBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IriGariBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/irigari/IriGariBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.irigari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IriGariBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
