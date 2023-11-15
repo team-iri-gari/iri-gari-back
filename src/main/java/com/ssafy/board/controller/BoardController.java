0b004ffcc3407bec67cfcd64f599353fde4c5b34
bohyeon9888 <bohyeon9888@gmail.com>
2023-11-15 14:29:45 +0900

    modify boardcontroller

--- src/main/java/com/ssafy/board/controller/BoardController.java
+++ src/main/java/com/ssafy/board/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.ssafy.board.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ssafy.board.model.FreeBoardDto;
 import com.ssafy.board.model.service.BoardService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/board")
 public class BoardController {
